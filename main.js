85469080cb2b1dead03d91e38c45216903c9d7c8
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-09-26 13:07:06 -0500

    update

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ var select = document.getElementById("PizzaType");
 function loadPizzas(){
     for(var i = 0; i < pizzaTypes.length; i++) {
         var option = document.createElement("option");
-        var t = pizzaTypes[i];
         console.log(pizzaTypes[i]);
         option.value = pizzaTypes[i];
         option.textContent = pizzaTypes[i];
-        select.appendChild(option,t);
-       
+        select.appendChild(option,pizzaTypes[i]);       
     }
 }
 
