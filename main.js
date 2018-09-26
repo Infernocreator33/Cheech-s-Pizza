2fc773fbadee8637923469104c46ba233a5836bb
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-09-26 13:00:35 -0500

    fixed loading selection from array

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
-const pizzaTypes = new Array["Pepperonni", "Sausage", "Supreme", "Taco", "Cheese"];
+const pizzaTypes = ["Pepperonni", "Sausage", "Supreme", "Taco", "Cheese"];
 var select = document.getElementById("PizzaType"); 
 
 function loadPizzas(){
     for(var i = 0; i < pizzaTypes.length; i++) {
         var option = document.createElement("option");
-        option.textContent(pizzaTypes[i]);
-        option.value(i);
-        select.appendChild(option);
+        var t = pizzaTypes[i];
+        console.log(pizzaTypes[i]);
+        option.value = pizzaTypes[i];
+        option.textContent = pizzaTypes[i];
+        select.appendChild(option,t);
+       
     }
 }
 
