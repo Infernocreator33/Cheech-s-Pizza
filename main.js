626ca087edb9c6ecb68388f6d55ac7c764c25334
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-09-26 11:27:16 -0500

    update

--- main.js
+++ main.js
@@ -1,16 +1,18 @@
 const pizzaTypes = new Array["Pepperonni", "Sausage", "Supreme", "Taco", "Cheese"];
 var select = document.getElementById("PizzaType"); 
 
-
+var fragment = document.createDocumentFragment();
 
 function loadPizzas(){
     for(var i = 0; i < pizzaTypes.length; i++) {
-        var type = pizzaTypes[i];
-        var option = document.createElement("option");
-        option.innerHTML = type;
-        option.value = type;
-        select(option);    
-    }​
+        var option = document.createElement("option"),
+            txt = document.createTextNode(pizzaTypes[i]);
+        option.appendChild(txt);
+        option.setAttribute("value", pizzaTypes[i]);
+        select.insertBefore(option, select.lastChild);
+    }
 }
+
+
 
 window.addEventListener("load", loadPizzas);
