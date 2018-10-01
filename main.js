73c23d49e926da582b8cc85b61ed48f60cbc812d
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-10-01 18:31:44 -0500

    update

--- main.js
+++ main.js
@@ -1,28 +1,38 @@
-const pizzaTypes = ["Pepperonni", 11, "Sausage", 11, "Supreme", 14, "Taco", 12, "Cheese", 10];
+const pizzaTypes = ["Pepperonni", 11.50, "Sausage", 11.50, "Supreme", 14.50, "Taco", 12.50, "Cheese", 10.50];
 var select = document.getElementById("PizzaType"); 
 var count = document.getElementById("TotalPizzas");  
-var firstName = document.getElementById("firstName").value;
-var lastName = document.getElementById("lastName").value;
-var phone = document.getElementById("phoneNumber").value;
-var submitOrder  = document.getElementById("submit");
+
+var phone = document.getElementById("phoneNumber");
+var submitOrder  = document.getElementById("submitOrder");
 var option = document.createElement("option");
 var custom = document.getElementById("custom");
 var addCustom = document.getElementById("addCustom");
-//submitOrder.addEventListener("click", printOrder);
-/*
-function printOrder()
-{
 
-}*/
+submitOrder.addEventListener("click", printOrder);
+
+function printOrder(){
+    var receipt = document.getElementById("receipt");
+    var fname = document.getElementById("firstName");
+    var lname = document.getElementById("lastName");
+    receipt.innerText = "Hi";   
+}
+
+function total(){
+    let subtotal = pizzaTypes[select.selectedIndex + 1] * count.selectedIndex.textContent;
+    let tax =  subtotal * .0076;
+    let total = subtotal + tax;
+    return total;
+}
 
 function setUpPage()
 {
     for(var i = 0; i < pizzaTypes.length; i+=2) {
         var option = document.createElement("option");
-        console.log(pizzaTypes[i]);
-        option.value = pizzaTypes[i];
-        option.textContent = pizzaTypes[i];
-        select.appendChild(option,pizzaTypes[i]);    
+        let temp = pizzaTypes[i] + "  " + "$" + pizzaTypes[i + 1].toFixed(2);
+        console.log(temp);
+        option.value = temp;
+        option.textContent = temp;
+        select.appendChild(option,temp);    
     }
     for(var i = 1; i < 11; i++){
         var option = document.createElement("option");
@@ -30,27 +40,6 @@ function setUpPage()
         option.textContent = i;
         count.appendChild(option, i);        
     }
-}
-
-function addCustomTopping(e,ele) {
-    var code = (e.keyCode ? e.keyCode : e.which);
-    if(code == 13) {
-        var option = document.createElement("option");
-        option.value = ele.value;
-        option.textContent = ele.value;
-        select.add(option);  
-        custom.value = ""; 
-        console.log(custom.value);
-    }
-}
-
-function addByButton(){
-    var option = document.createElement("option");    
-    option.value = custom.value;
-    option.textContent = custom.value;
-    select.add(option);  
-    custom.value = ""; 
-    console.log(custom.value);    
 }
 
 window.addEventListener("load", setUpPage);
