931df9214d6d9fc9b7479a3e3a78aeb4bac5b018
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-10-01 21:17:27 -0500

    finishing the receipt

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const pizzaTypes = ["Pepperonni", 11.50, "Sausage", 11.50, "Supreme", 14.50, "Taco", 12.50, "Cheese", 10.50];
+const pizzaTypes = ["Pepperonni","Sausage", "Supreme", "Taco", "Cheese"];
+const pizzaCost = [11.50,11.50,14.50,12.50,10.50];
 var select = document.getElementById("PizzaType"); 
 var count = document.getElementById("TotalPizzas");  
 var receipt = document.getElementById("receipt");
@@ -15,24 +16,25 @@ submitOrder.addEventListener("click", printOrder);
 function printOrder(){    
     let index = select[select.selectedIndex].value;
     let t = count[count.selectedIndex].value;
-    let cost = pizzaTypes[1] ;
+    let cost = pizzaCost[index] ;
     let subtotal = t * cost;
     let tax =  subtotal * .076;
     let total = subtotal + tax;
-    receipt.value = "Customer: " + fname.value + ' ' + lname.value + "\n" + "Email: " + email.value + "\n" 
-        + "Phone Number: " + phone.value + "\n\n\n"; 
-
-    receipt.value += "Subtotal: " + subtotal + "\n" + "Tax: $" + tax + "\nTotal: $" + total ;
+    receipt.value = "\n\n\nCustomer: " + fname.value + ' ' + lname.value + "\n" + "Email: " + email.value + "\n" 
+        + "Phone Number: " + phone.value + "\n\n\n\n" + "Type of Pizza: " + pizzaTypes[index] + " x" + t + "\n"+ "Crust: " 
+        + document.querySelector('input[name="crust"]:checked').value + "\n" 
+        + document.querySelector('input[name="pickupOption"]:checked').value +"\n\n\n" + "Subtotal: $" 
+        + subtotal.toFixed(2) + "\n" + "Tax: $" + tax.toFixed(2) + "\nTotal: $" + total.toFixed(2); 
 }
 
 function setUpPage()
 {
-    for(var i = 0; i < pizzaTypes.length; i+=2) {
+    for(var i = 0; i < pizzaTypes.length; i++) {
         var option = document.createElement("option");
-        let temp = pizzaTypes[i] + "  " + "$" + pizzaTypes[i + 1].toFixed(2);
+        let temp = pizzaTypes[i] + "  " + "$" + pizzaCost[i].toFixed(2);
         console.log(temp);
         option.value = i;
-        option.textContent = pizzaTypes[i];
+        option.textContent = temp;
         select.appendChild(option,temp);    
     }
     for(var i = 1; i < 11; i++){
