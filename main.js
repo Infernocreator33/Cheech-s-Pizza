450dee5359d770370fafe3523e2060afcb543870
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-10-01 20:36:13 -0500

    created receipt

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const pizzaTypes = ["Pepperonni", 11.50, "Sausage", 11.50, "Supreme", 14.50, "Taco", 12.50, "Cheese", 10.50];
 var select = document.getElementById("PizzaType"); 
 var count = document.getElementById("TotalPizzas");  
-
+var receipt = document.getElementById("receipt");
+var fname = document.getElementById("firstName");
+var lname = document.getElementById("lastName");
 var phone = document.getElementById("phoneNumber");
 var submitOrder  = document.getElementById("submitOrder");
 var option = document.createElement("option");
 var custom = document.getElementById("custom");
 var addCustom = document.getElementById("addCustom");
-
+var email = document.getElementById("email");
 submitOrder.addEventListener("click", printOrder);
 
-function printOrder(){
-    var receipt = document.getElementById("receipt");
-    var fname = document.getElementById("firstName");
-    var lname = document.getElementById("lastName");
-    receipt.innerText = "Hi";   
-}
-
-function total(){
-    let subtotal = pizzaTypes[select.selectedIndex + 1] * count.selectedIndex.textContent;
-    let tax =  subtotal * .0076;
+function printOrder(){    
+    let index = select[select.selectedIndex].value;
+    let t = count[count.selectedIndex].value;
+    let cost = pizzaTypes[1] ;
+    let subtotal = t * cost;
+    let tax =  subtotal * .076;
     let total = subtotal + tax;
-    return total;
+    receipt.value = "Customer: " + fname.value + ' ' + lname.value + "\n" + "Email: " + email.value + "\n" 
+        + "Phone Number: " + phone.value + "\n\n\n"; 
+
+    receipt.value += "Subtotal: " + subtotal + "\n" + "Tax: $" + tax + "\nTotal: $" + total ;
 }
 
 function setUpPage()
@@ -30,8 +31,8 @@ function setUpPage()
         var option = document.createElement("option");
         let temp = pizzaTypes[i] + "  " + "$" + pizzaTypes[i + 1].toFixed(2);
         console.log(temp);
-        option.value = temp;
-        option.textContent = temp;
+        option.value = i;
+        option.textContent = pizzaTypes[i];
         select.appendChild(option,temp);    
     }
     for(var i = 1; i < 11; i++){
