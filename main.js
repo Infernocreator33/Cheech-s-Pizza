a9bc607a5898034e218dd332ec22becac7c0637a
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-10-02 14:19:32 -0500

    finished

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const pizzaTypes = ["Pepperonni","Sausage", "Supreme", "Taco", "Cheese"];
-const pizzaCost = [11.50,11.50,14.50,12.50,10.50];
+const pizzaTypes = ["Pepperonni","Sausage", "Supreme", "Taco", "Ham", "Cheese"];
+const pizzaCost = [11.50,11.50,14.50,12.50,10.50, 9.50];
 var select = document.getElementById("PizzaType"); 
 var count = document.getElementById("TotalPizzas");  
 var receipt = document.getElementById("receipt");
@@ -20,7 +20,8 @@ function printOrder(){
     let subtotal = t * cost;
     let tax =  subtotal * .076;
     let total = subtotal + tax;
-    receipt.value = "\n\n\n\n\n\n\n\nCustomer: " + fname.value + ' ' + lname.value + "\n" + "Email: " + email.value + "\n" 
+    receipt.value = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nCustomer: " 
+        + fname.value + ' ' + lname.value + "\n" + "Email: " + email.value + "\n" 
         + "Phone Number: " + phone.value + "\n\n\n\n" + "Type of Pizza: " + pizzaTypes[index] + " x" + t + "\n"
         + "Crust: " + document.querySelector('input[name="crust"]:checked').value + "\n" 
         + document.querySelector('input[name="pickupOption"]:checked').value +"\n\n\n\n" + "Subtotal: $" 
@@ -31,7 +32,7 @@ function setUpPage()
 {
     for(var i = 0; i < pizzaTypes.length; i++) {
         var option = document.createElement("option");
-        let temp = pizzaTypes[i] + "  " + "$" + pizzaCost[i].toFixed(2);
+        var temp = pizzaTypes[i] + "  " + "$" + pizzaCost[i].toFixed(2);
         console.log(temp);
         option.value = i;
         option.textContent = temp;
